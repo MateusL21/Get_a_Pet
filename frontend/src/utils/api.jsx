21785aa1f8681834844ef41a8baeb6bed2c24882
MateusL21 <mateusleonel27@gmail.com>
2025-10-21 14:38:12 -0300

    fix

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// URL base dinâmica - funciona em ambos os ambientes
+const baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: baseURL,
 });
